61d68cfb3f7bc2b81700c2df354d32943d48fc80
Nidhi5596 <166806211+Nidhi5596@users.noreply.github.com>
2024-04-12 16:00:14 +0100

    E&D.py

--- E&D.py
+++ E&D.py
@@ -1,20 +1,26 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-text = input("Enter Your Message: ")
-shift = int(input("Enter Your Shift Amount: "))
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
         if new_position > 25:
-            new_position2 = new_position - 26
-            new_letter2 = alphabet[new_position2]
-            cipher_text += new_letter2
-        else:
-            new_letter1 = alphabet[new_position]
-            cipher_text += new_letter1
-    print(f"The Encrypted Message is: {cipher_text}")
+            new_position -= 26
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encrypted message is: {cipher_text}")
 
-encrypt(plain_text=text, shift_amount=shift)
+plain_text = input("Enter Your Message: ").lower()
+shift = int(input("Enter your shift Amount: "))
+encrypt(plain_text, shift)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The Decrypted Message is: {plain_text}")
+
+cipher_text = input("Enter the Encrypted Message to be Decrypted: ").lower()
+decrypt(cipher_text, shift)
 
